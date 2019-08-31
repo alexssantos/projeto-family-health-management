1f7b8c029deeb910f5d1642555cf4c1bed185933
aarkanjo <arkanjo.alex@gmail.com>
2019-08-31 16:01:44 -0300

    GSF - API - first Controller

---
+++ gsf.api/src/main/java/com/seventeam/gsf/controllers/LoginController.java
@@ -0,0 +1,17 @@
+package com.seventeam.gsf.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/login")
+public class LoginController
+{
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String Login()
+    {
+        return "Olá, Usuário!";
+    }
+}
