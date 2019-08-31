0f640c3866f660504c5c0b22479e5f797c9ebfc0
aarkanjo <arkanjo.alex@gmail.com>
2019-08-31 13:56:18 -0300

    start projeject GSF - API

---
+++ gsf.api/src/main/java/com/seventeam/gsf/GsfApplication.java
@@ -0,0 +1,13 @@
+package com.seventeam.gsf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GsfApplication.class, args);
+    }
+
+}
