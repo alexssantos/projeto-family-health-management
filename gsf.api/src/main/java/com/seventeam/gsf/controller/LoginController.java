09e5b2d101c005a6bc9b7fe7abd49632393081fe
aarkanjo <arkanjo.alex@gmail.com>
2019-09-02 20:32:12 -0300

    Getting Parameter on Controller

--- gsf.api/src/main/java/com/seventeam/gsf/controller/LoginController.java
+++ gsf.api/src/main/java/com/seventeam/gsf/controller/LoginController.java
@@ -3,18 +3,21 @@ package com.seventeam.gsf.controller;
 import org.apache.coyote.Request;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping(value = "/api/login")
 public class LoginController
 {
 
     @RequestMapping(method = RequestMethod.GET)
-    public @ResponseBody String Login(@RequestParam("user") String user,
-                                      @RequestParam("password") String password
-    ){
-        String params = user;
-        String pass = password;
+    public @ResponseBody String Login(@RequestParam Map<String, String> allRequestParams){
+        String user = allRequestParams.get("user");
+        String pass = allRequestParams.get("password");
+        String erro = allRequestParams.get("erro");     //DONT THROW exception. null value.
 
         return "Olá, Usuário!";
     }
+
+    // public @ResponseBody String Login(@RequestParam("user") String user, @RequestParam("password") String password)
 }
