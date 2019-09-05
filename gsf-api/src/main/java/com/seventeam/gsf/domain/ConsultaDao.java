0daf224fa2d1fb3ac0dd26d6f9ef0a393f56d61f
aarkanjo <arkanjo.alex@gmail.com>
2019-09-04 23:53:53 -0300

    GSF - Consulta ENTITY + REPO

--- gsf-api/src/main/java/com/seventeam/gsf/domain/ConsultaDao.java
+++ gsf-api/src/main/java/com/seventeam/gsf/domain/ConsultaDao.java
@@ -1,4 +1,68 @@
 package com.seventeam.gsf.domain;
 
-public class ConsultaDao {
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@JsonIgnoreProperties(value = {"createdAt", "updatedAt"}, allowGetters = true)
+@Entity
+@Table(name = "consulta")
+public class ConsultaDao implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    private String Tipo;
+    private Date DataPrevista;
+    private Date DataFeita;
+    private Long Consulta_ID;
+
+    public ConsultaDao() {
+    }
+
+    public ConsultaDao(String tipo, Date dataPrevista, Date dataFeita, Long consulta_ID) {
+        Tipo = tipo;
+        DataPrevista = dataPrevista;
+        DataFeita = dataFeita;
+        Consulta_ID = consulta_ID;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTipo() {
+        return Tipo;
+    }
+
+    public void setTipo(String tipo) {
+        Tipo = tipo;
+    }
+
+    public Date getDataPrevista() {
+        return DataPrevista;
+    }
+
+    public void setDataPrevista(Date dataPrevista) {
+        DataPrevista = dataPrevista;
+    }
+
+    public Date getDataFeita() {
+        return DataFeita;
+    }
+
+    public void setDataFeita(Date dataFeita) {
+        DataFeita = dataFeita;
+    }
+
+    public Long getConsulta_ID() {
+        return Consulta_ID;
+    }
+
+    public void setConsulta_ID(Long consulta_ID) {
+        Consulta_ID = consulta_ID;
+    }
 }
---
+++ gsf-api/src/main/java/com/seventeam/gsf/repository/ConsultaRepository.java
@@ -0,0 +1,7 @@
+package com.seventeam.gsf.repository;
+
+import com.seventeam.gsf.domain.ConsultaDao;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConsultaRepository extends JpaRepository<ConsultaDao, Long> {
+}
