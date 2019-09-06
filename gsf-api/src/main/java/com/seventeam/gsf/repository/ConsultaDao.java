8eba4c6a7c5cc2497619428de5d5cff7d0e35c49
aarkanjo <arkanjo.alex@gmail.com>
2019-09-06 01:10:01 -0300

    REMOVE Consulta Entity

--- gsf-api/src/main/java/com/seventeam/gsf/repository/ConsultaDao.java
+++
@@ -1,9 +0,0 @@
-package com.seventeam.gsf.repository;
-
-import com.seventeam.gsf.domain.Consulta;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ConsultaDao extends JpaRepository<Consulta, Integer> {
-}
