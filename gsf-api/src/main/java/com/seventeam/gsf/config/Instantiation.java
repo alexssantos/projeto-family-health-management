f612105e5f976fe6998d6dcf27624c0e675cf9e8
aarkanjo <arkanjo.alex@gmail.com>
2019-09-07 18:14:14 -0300

    BASE - migration - refactor table medico

--- gsf-api/src/main/java/com/seventeam/gsf/config/Instantiation.java
+++ gsf-api/src/main/java/com/seventeam/gsf/config/Instantiation.java
@@ -1,8 +1,11 @@
 package com.seventeam.gsf.config;
 
+import com.seventeam.gsf.Utils.UtilsString;
+import com.seventeam.gsf.domain.Medico;
 import com.seventeam.gsf.domain.Paciente;
 import com.seventeam.gsf.domain.Usuario;
 import com.seventeam.gsf.models.enums.EnumUsuarioPerfil;
+import com.seventeam.gsf.repository.MedicoDao;
 import com.seventeam.gsf.repository.PacienteDao;
 import com.seventeam.gsf.repository.UsuarioDao;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,9 +24,10 @@ public class Instantiation implements CommandLineRunner {
 
     @Autowired
     private PacienteDao pacienteDao;
-
     @Autowired
     private UsuarioDao usuarioDao;
+    @Autowired
+    private MedicoDao medicoDao;
 
     public static List<Usuario> usuarioList = new ArrayList<>();
     public static List<Paciente> pacienteList = new ArrayList<>();
@@ -35,8 +39,9 @@ public class Instantiation implements CommandLineRunner {
 
         sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 
-        //pushUsuarioToDb();
+        pushUsuarioToDb();
         pushPacienteToDb();
+        pusuMedicoToDb();
     }
 
 
@@ -55,21 +60,58 @@ public class Instantiation implements CommandLineRunner {
 
     private void pushPacienteToDb() throws Exception
     {
-        pacienteDao.deleteAll();
 
-        Usuario alexUser = new Usuario("alex@gmail.com", "123", EnumUsuarioPerfil.MEDICO);
+        Usuario alexUser = new Usuario("alex@gmail.com", "123", EnumUsuarioPerfil.PACIENTE);
         Paciente alex = new Paciente("Alex Santos", sdf.parse("25/10/2019"),sdf.parse("30/10/2019"), alexUser);
 
-        //Paciente bruna = new Paciente("Bruna Dolavale", sdf.parse("01/05/2019"),sdf.parse("15/05/2019"));
-        //Paciente matheus = new Paciente("Matheus Gomes", sdf.parse("02/06/2019"),sdf.parse("16/06/2019"));
-        //Paciente thais = new Paciente("Thais Machado", sdf.parse("03/07/2019"),sdf.parse("17/07/2019"));
+        Usuario brunaUser = new Usuario("bruna@gmail.com", "123", EnumUsuarioPerfil.PACIENTE);
+        Paciente bruna = new Paciente("Bruna Dolavale", sdf.parse("01/05/2019"),sdf.parse("15/05/2019"), brunaUser);
 
-        pacienteDao.save(alex);
-//        pacienteDao.saveAll(Arrays.asList(
-//                alex
-//                //bruna,
-//                //matheus,
-//                //thais
-//        ));
+        Usuario matheusUser = new Usuario("matheus@gmail.com", "123", EnumUsuarioPerfil.PACIENTE);
+        Paciente matheus = new Paciente("Matheus Gomes", sdf.parse("02/06/2019"),sdf.parse("16/06/2019"), matheusUser);
+
+        Usuario thaisUser = new Usuario("thais@gmail.com", "123", EnumUsuarioPerfil.PACIENTE);
+        Paciente thais = new Paciente("Thais Machado", sdf.parse("03/07/2019"),sdf.parse("17/07/2019"), thaisUser);
+
+        try
+        {
+            pacienteDao.deleteAll();
+
+            pacienteDao.saveAll(Arrays.asList(
+                    alex, bruna, matheus, thais
+            ));
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void pusuMedicoToDb()
+    {
+        List<Medico> medicoList = new ArrayList<>();
+        int qtdMedicos = 10;
+
+        for (int i=0; i<qtdMedicos; i++)
+        {
+            Medico medicoX = new Medico(
+                    UtilsString.msgFormat("Medico-{0}",i),
+                    new Usuario(
+                            UtilsString.msgFormat("medico-{0}@gmail.com",i),
+                            "123",
+                            EnumUsuarioPerfil.MEDICO)
+            );
+
+            medicoList.add(medicoX);
+        }
+
+        try
+        {
+            medicoDao.deleteAll();
+
+            medicoDao.saveAll(medicoList);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
