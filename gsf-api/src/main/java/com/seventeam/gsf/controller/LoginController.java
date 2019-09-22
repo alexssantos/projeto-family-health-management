048a255a00d9506965f3afd7cc034bf4163185df
alexssantos <arkanjo.alex@gmail.com>
2019-09-22 11:35:11 -0300

    ngLogin - done - (testing)

--- gsf-api/src/main/java/com/seventeam/gsf/controller/LoginController.java
+++ gsf-api/src/main/java/com/seventeam/gsf/controller/LoginController.java
@@ -1,5 +1,6 @@
 package com.seventeam.gsf.controller;
 
+import com.seventeam.gsf.Utils.UtilsString;
 import com.seventeam.gsf.controller.validate.ValidateForms;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,14 +22,15 @@ public class LoginController
     public @ResponseBody String Login(@RequestParam Map<String, String> Params){
         
         //validate Login Parans ->
-        if (ValidateForms.validateLogin(Params)){
+        if (!ValidateForms.validateLogin(Params)){
             return "VACILO";
         }
         String user = Params.get("user");
         String pass = Params.get("password");
         String tipoPerfil = Params.get("perfil");
-
-        return "Olá, Usuário!";
+    
+        String reponseStr = UtilsString.msgFormat("LOGADO. \nOlá, '{0}'! \n{0}\t{1}\t{2}", user, pass,tipoPerfil);
+        return reponseStr;
     }
 
     // public @ResponseBody String Login(@RequestParam("user") String user, @RequestParam("password") String password)
